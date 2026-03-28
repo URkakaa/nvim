c0a1d054e591f33c585903120149ad186d1d6b67
URkakaa <2754863397@qq.com>
2026-03-28 09:47:13 +0800

    end

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- 拼写检查
+vim.opt.spelllang = { "en", "cjk" }
+
+-- 设置Neovim默认shell为fish
+vim.opt.shell = "fish"
+-- 可选：关闭shell参数（避免fish解析错误）
+vim.opt.shellcmdflag = "-c"
